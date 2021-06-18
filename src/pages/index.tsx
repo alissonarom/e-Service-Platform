0ce96d60d47350d39c42f6843c0898e54a9d8978
Alisson Morais <alissonmorais.br@gmail.com>
2021-06-18 11:22:20 -0300

    Refresh start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
   <div>
     <SafeEnvironment/>
-    <Text>Atualizção</Text>
+    Atualização
   </div>
   )
 }
---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeEnvironmentContainer } from './SafeEnvironment.style';
+
+const SafeEnvironment = () => {
+    return (
+    <SafeEnvironmentContainer>
+        ambiente seguro
+    </SafeEnvironmentContainer>
+    )
+}
+
+export default SafeEnvironment;
